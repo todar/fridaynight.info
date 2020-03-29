7f8c6cd730aa0d3dcde248822963230028fa7840
robert todar <robert@roberttodar.com>
2020-03-29 00:27:56 -0700

    Added some basic content for prototyping

--- src/pages/connect.jsx
+++ src/pages/connect.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const connect = () => {
-  return <div>Connect</div>;
+  return (
+    <Container maxWidth="md">
+      <Card>
+        <CardContent>
+          <Typography variant="h2">Content Coming Soon</Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default connect;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,64 @@
  * is to display our main verses and any call to action (none at this time).
  * Mostly, this app is about making the songbook digital.
  */
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+const sectionStyle = {
+  backgroundColor: "rgb(31, 32, 35)",
+  background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("./showcase.jpg")`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  color: "#fff",
+  marginTop: "-2.5rem",
+  height: "100vh"
+};
+
 const HomePage = () => {
-  return <h1>HomePage Test</h1>;
+  return (
+    <section style={sectionStyle}>
+      <Container maxWidth="md">
+        <Box py={8}>
+          <Card color="primary">
+            <CardContent>
+              <Typography variant="subtitle1" gutterBottom>
+                1 Corinthians 14:26
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                "What is the outcome then, brethren? When you assemble, each one
+                has a psalm, has a teaching, has a revelation, has a tongue, has
+                an interpretation. Let all things be done for edification."
+              </Typography>
+              <Typography variant="caption" gutterBottom>
+                New American Standard Bible
+              </Typography>
+            </CardContent>
+          </Card>
+          <Box mt={2}>
+            <Card>
+              <CardContent>
+                <Typography variant="subtitle1" gutterBottom>
+                  John 13:35
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  "By this all men will know that you are My disciples, if you
+                  have love for one another."
+                </Typography>
+                <Typography variant="caption" gutterBottom>
+                  New American Standard Bible
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        </Box>
+      </Container>
+    </section>
+  );
 };
 
 export default HomePage;
