eaa1a333ef7b13933e60233559d8eadc5ed24dfb
robert todar <robert@roberttodar.com>
2020-03-28 23:03:08 -0700

    Added theme colors to NavBar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,73 +5,67 @@ import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import HomeIcon from "@material-ui/icons/Home";
 import SongsIcon from "@material-ui/icons/MusicNote";
 import ConnectIcon from "@material-ui/icons/LocationOn";
-import { styled } from "@material-ui/core/styles";
+import { styled, useTheme } from "@material-ui/core/styles";
+import NoSsr from "@material-ui/core/NoSsr";
 
 const StyledBottomNavigation = styled(BottomNavigation)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.secondary.main,
+  position: "fixed",
+  bottom: 0,
+  left: 0,
+  right: 0,
+  width: "100%"
 }));
 
-const StyledBottomNavigationAction = styled(BottomNavigationAction)(
-  ({ theme }) => ({
-    color: theme.palette.secondary.main
-  })
-);
+// @TODO Try to get this to work???
+const NavAction = styled(BottomNavigationAction)(({ theme }) => ({
+  root: {
+    color: theme.palette.secondary.dark,
+    "&$selected": {
+      color: theme.palette.secondary.main
+    }
+  }
+}));
 
 const navbar = () => {
   const [value, setValue] = React.useState(0);
+  const theme = useTheme();
 
   return (
-    <StyledBottomNavigation
-      style={bottomNavStyle}
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-    >
-      <BottomNavigationAction
-        component={Link}
-        href="/"
-        label="Home"
-        icon={<HomeIcon />}
-      />
+    <NoSsr>
+      <StyledBottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction
+          style={{ color: theme.palette.secondary.dark }}
+          component={Link}
+          href="/"
+          label="Home"
+          icon={<HomeIcon />}
+        />
 
-      <BottomNavigationAction
-        component={Link}
-        href="/songs"
-        label="Songs"
-        icon={<SongsIcon />}
-      />
-      <BottomNavigationAction
-        component={Link}
-        href="/connect"
-        label="Connect"
-        icon={<ConnectIcon />}
-      />
-    </StyledBottomNavigation>
+        <BottomNavigationAction
+          style={{ color: theme.palette.secondary.dark }}
+          component={Link}
+          href="/songs"
+          label="Songs"
+          icon={<SongsIcon />}
+        />
+        <BottomNavigationAction
+          style={{ color: theme.palette.secondary.dark }}
+          component={Link}
+          href="/connect"
+          label="Connect"
+          icon={<ConnectIcon />}
+        />
+      </StyledBottomNavigation>
+    </NoSsr>
   );
 };
 
 export default navbar;
-
-const bottomNavStyle = {
-  position: "fixed",
-  bottom: 0,
-  width: "100%"
-};
-
-{
-  /* <div>
-<nav>
-  <Link href="/">
-    <a>Home</a>
-  </Link>
-  <Link href="/songs">
-    <a>Songs</a>
-  </Link>
-  <Link href="/connect">
-    <a>Connect</a>
-  </Link>
-</nav>
-</div> */
-}
