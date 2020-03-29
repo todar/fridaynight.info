08a69641d89c6c2da6ddc483cd9dc999fd524118
robert todar <robert@roberttodar.com>
2020-03-28 23:08:53 -0700

    Refactored the apps theme into it's own file

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,24 +4,10 @@
  * of each page for navigation and the placement of the rest of the app above.
  */
 import React from "react";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Navbar from "./navbar";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#1e1f20",
-      light: "#4b4b4c",
-      dark: "#151516"
-    },
-    secondary: {
-      main: "#ffd600",
-      light: "#ffde33",
-      dark: "#b29500"
-    }
-  }
-});
+import theme from "../theme";
 
 const Layout = ({ children }) => {
   return (
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1e1f20",
+      light: "#4b4b4c",
+      dark: "#151516"
+    },
+    secondary: {
+      main: "#ffd600",
+      light: "#ffde33",
+      dark: "#b29500"
+    }
+  }
+});
+
+export default theme;
